bac9be53321bae018997257a49fcd75e0ac07863
Lassi Heikkila <lassi.heikkila@fara.fi>
2020-07-28 11:58:02 +0300

    Sorting weekly menu entries according to category, same way as was done for daily menu

--- pretty_printing.go
+++ pretty_printing.go
@@ -1,36 +1,42 @@
 package main
 
 import (
-    "fmt"
-    "sort"
+	"fmt"
+	"sort"
 )
 
 func prettyPrintDailyList(l *DailyList) {
-    if l == nil {
-        return
-    }
-    fmt.Printf("Daily Menu for %s\n", l.Meta.RefTitle)
+	if l == nil {
+		return
+	}
+	fmt.Printf("Daily Menu for %s\n", l.Meta.RefTitle)
 
-    var keys []string
-    for n := range l.Courses {
-        keys = append(keys, n)
-    }
-    sort.Strings(keys)
-    for _, key := range keys {
-        course := l.Courses[key]
-        fmt.Printf("[%s] option %s: %s\n", course.Category, key, course.TitleFi)
-    }
+	var keys []string
+	for n := range l.Courses {
+		keys = append(keys, n)
+	}
+	sort.Strings(keys)
+	for _, key := range keys {
+		course := l.Courses[key]
+		fmt.Printf("[%s] option %s: %s\n", course.Category, key, course.TitleFi)
+	}
 }
 
 func prettyPrintWeeklyList(l *WeeklyList) {
-    if l == nil {
-        return
-    }
-    fmt.Printf("Weekly Menu for %s\n", l.Meta.RefTitle)
-    for _, m := range l.MealDates {
-        fmt.Printf("Options on %s\n", m.Date)
-        for n, course := range m.Courses {
-            fmt.Printf("\t[%s] %s: %s\n", course.Category, n, course.TitleFi)
-        }
-    }
+	if l == nil {
+		return
+	}
+	fmt.Printf("Weekly Menu for %s\n", l.Meta.RefTitle)
+	for _, m := range l.MealDates {
+		fmt.Printf("Options on %s\n", m.Date)
+		var keys []string
+		for n := range m.Courses {
+			keys = append(keys, n)
+		}
+		sort.Strings(keys)
+		for _, key := range keys {
+			course := m.Courses[key]
+			fmt.Printf("\t[%s] %s: %s\n", course.Category, key, course.TitleFi)
+		}
+	}
 }
